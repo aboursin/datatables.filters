cb94d04c20e6240bc4c18ad5e3d65a21daf243ab
aboursin <angelo.boursin@gmail.com>
2016-02-18 09:29:07 +0100

    DataTables Filters Plugin

---
+++ README.md
@@ -0,0 +1,18 @@
+# DataTables Filters Plugin
+
+This [DataTables] plugin add a filter input on each DataTable columns.
+
+![Alt text](https://cloud.githubusercontent.com/assets/16703873/13043470/9efe16be-d3c8-11e5-8a54-a36b1b975a8a.png)
+
+## Example
+
+``` javascript
+$(document).ready(function() {
+
+	// Initialize DataTable and active filter plugin
+	$('#example').DataTable().filtersOn();
+
+} );
+```
+
+   [DataTables]: <https://datatables.net/>
---
+++ datatables.filters.js
@@ -0,0 +1,87 @@
+/**
+ * Filter each DataTable columns. The plugin add a simple text input at 
+ * the top of each columns.
+ *
+ * TODO : Add the option to choose the type of input (text, select, 
+ * range...)
+ *
+ *  @summary Add a filter input on each DataTable columns
+ *  @name DataTables Filters
+ *  @author angelo.boursin
+ *
+ *  @example
+ *		$(document).ready(function() {
+ *		
+ *			// Initialize DataTable and active filter plugin
+ *			$('#example').DataTable().filtersOn();
+ *
+ *    } );
+ */
+
+$.fn.dataTable.Api.register( 'filtersOn()', function () {
+	
+	var dataTable = this;
+	var id = $(dataTable.context[0].nTable).attr('id');
+	var state = dataTable.state.loaded();
+
+	// Create the filter header row
+	$('#' + id + ' thead').after(
+		$('<thead />').addClass('filter').append($('<tr />'))
+	);
+	
+	// Populate the filter header
+	$('#' + id + ' thead th').each( function (index) {
+		var searchable = dataTable.context[0].aoColumns[index].bSearchable;
+		
+		// Add input only if current column is searchable
+		if(searchable){
+			$('#' + id + ' .filter tr').append($('<th>').append($('<input type="text"/>').addClass('form-control input-sm')));
+		} else {
+			$('#' + id + ' .filter tr').append($('<th>'));
+		}
+	});
+	
+	// Restore filter (only if stateSave)
+    if (state) {
+    	console.log("stateSave:true > Restoring filters...");
+    	dataTable.columns().eq(0).each( function (index) {
+        var colSearch = state.columns[index].search;
+        if (colSearch.search) {
+        	// Restore input value
+        	$('#' + id + ' .filter input').get(index).value = colSearch.search;
+        }
+      });
+    }
+    
+ 	// Filter input event : filter matching column
+	$('#' + id +' .filter input').each( function (index) {
+		$(this).on( 'keyup change', function () {
+			dataTable.column(index).search(this.value).draw();
+		});
+	});
+	
+	return dataTable;
+	
+});
+
+$.fn.dataTable.Api.register( 'filtersClear()', function () {
+	
+	var dataTable = this;
+	var id = $(dataTable.context[0].nTable).attr('id');
+	var state = dataTable.state.loaded();
+	
+	// Clean filters (only if stateSave)
+	if (state) {
+		console.log("stateSave:true > Clearing filters...");
+		$('#' + id +' .filter input').each( function (index) {
+			// Clear input value
+			this.value = '';
+			// Clear column filter
+			dataTable.column(index).search('');
+		});
+	
+		// Re-draw datatable
+  		dataTable.draw();
+	}
+	
+});
